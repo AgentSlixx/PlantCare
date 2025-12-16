e78d2239a0983fd4b62d49ae78bd757bee1d2493
AgentSlixx <mussettg@icloud.com>
2025-12-16 10:25:05 +0000

    Fixed UI/Main issue by moving ui functions to src

--- src/main.py
+++ src/main.py
@@ -1,8 +1,3 @@
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from ui_main import ui_run
 
-from ui.ui_main import ui_run
-
-while 1 != 2:
-    ui_run()
+ui_run()
