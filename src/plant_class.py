e10fdaab86f519b61e699ee5197bb4c237f49006
AgentSlixx <mussttg@icloud.com>
2025-12-31 17:28:11 +0000

    Added a class to create plant blueprints

---
+++ src/plant_class.py
@@ -0,0 +1,8 @@
+class Plant:
+    def __init__(self, name, light, temperature, humidity, moisture):
+        self.name = name
+        self.light_values = light
+        self.temperature_values = temperature
+        self.humidity_values = humidity
+        self.moisture_values = moisture
+    
