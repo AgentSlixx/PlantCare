a4f1e437e1d84404d9135102bb973d162026b1ac
AgentSlixx <mussettg@icloud.com>
2026-01-09 10:49:48 +0000

    stuff

--- src/plantbook_api.py
+++ src/plantbook_api.py
@@ -1,4 +1,5 @@
 import requests
+from plant_class import Plant
 
 Base_URL = "https://open.plantbook.io/api/v1/"
 MY_CLIENT_ID = "qOKVybGCvVCFBK7FGdN4RFbnpdgVbNY5RlCv4eWN" #MY CLIENT ID AND SECRET, MAKE USER INPUT THEIRS
@@ -40,7 +41,23 @@ class PlantbookAPI:
         response.raise_for_status()
         return response.json()
     
-if __name__ == "__main__":
+    def get_plant_light_limits(self, pid):
+        plant_detail = self.get_plant_detail(pid)
+        return plant_detail.get("min_light_lux"), plant_detail.get("max_light_lux")
+
+    def get_plant_temperature_limits(self, pid):
+        plant_detail = self.get_plant_detail(pid)
+        return plant_detail.get("min_temp"), plant_detail.get("max_temp")
+    
+    def get_plant_humidity_limits(self, pid):
+        plant_detail = self.get_plant_detail(pid)
+        return plant_detail.get("min_env_humid"), plant_detail.get("max_env_humid")
+
+    def get_plant_moisture_limits(self, pid):
+        plant_detail = self.get_plant_detail(pid)
+        return plant_detail.get("min_soil_moist"), plant_detail.get("max_soil_moist")
+
+while __name__ == "__main__":
     
     user_Client_ID = input("Enter your Plantbook Client ID: ")
     user_client_secret = input("Enter your Plantbook Client Secret: ")
@@ -51,12 +68,24 @@ if __name__ == "__main__":
 
     api = PlantbookAPI(Client_ID, Client_SECRET)
 
-    #Search for plant
-    results = api.search_plant(plant_name)
-    if not results:
-        print(f"No plant found for '{plant_name}'")
-        exit()
+    #get the plant max and min light, temperature, humidity, moisture and save to a Plant object
+    search_results = api.search_plant(plant_name)
+    if not search_results:
+        print("No plants found with that name.")
+    else:
+        plant_light_limits = api.get_plant_light_limits(search_results[0]['pid'])
+        plant_temperature_limits = api.get_plant_temperature_limits(search_results[0]['pid'])
+        plant_humidity_limits = api.get_plant_humidity_limits(search_results[0]['pid'])
+        plant_moisture_limits = api.get_plant_moisture_limits(search_results[0]['pid'])
 
-    pid = results[0]["pid"]
- 
- #DOESN'T RETURN VALUES PROPERLY, FIX
+        print(f"Plant '{plant_name}' added with the following limits:")
+        print(f"Light limits: {plant_light_limits}")
+        print(f"Temperature limits: {plant_temperature_limits}")
+        print(f"Humidity limits: {plant_humidity_limits}")
+        print(f"Moisture limits: {plant_moisture_limits}")
+
+        user_plant = Plant(plant_name, plant_light_limits, plant_temperature_limits, plant_humidity_limits, plant_moisture_limits)
+
+        
+        # add plant info to users.json under the user's plants list
+        # WORK IN PROGRESS
